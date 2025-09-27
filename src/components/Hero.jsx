e1e68c58959ed1ffe67c55e98110f9aa3ad4ffd1
smsohelrana420 <mirzasohel550@gmail.com>
2025-09-27 13:19:40 +0600

    category done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -17,9 +17,126 @@ const Hero = () => {
     slidesToScroll: 1,
     arrowsL:false
   };
+
+  useEffect(()=>{
+    AOS.init({
+      offset:100,
+      duration:500,
+      easing:"ease-in-out"
+    });
+    AOS.refresh();
+  },[])
   return (
-    <div>Hero</div>
-  )
+    <div
+      id="hero"
+      className="w-full flex justify-center items-center lg:h-[700px] h-[600px]"
+    >
+      <Slider className="w-full " {...settings}>
+        <div>
+          <div
+            className="w-full lg:px-20 px-5 lg:h-[700px] h-[600px] flex flex-col justify-center items-start gap-10 bg-cover bg-center "
+            style={{ backgroundImage: `url(${dslr})` }}
+          >
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="50"
+              className="text-themeyellow border rounded-lg border-themeyellow px-6 py-2 text-xl"
+            >
+              Get upto Discounts 80% off
+            </h1>
+            <h2
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white lg:text-[120px] text-[60px] font-bold uppercase lg:leading-[120px] leading-[70px]"
+            >
+              DSLR 360 <br /> Camera
+            </h2>
+            <h2
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white text-2xl"
+            >
+              100% trusted{" "}
+              <span className="text-themeyellow font-semibold">
+                Electronic Gadgets
+              </span>
+            </h2>
+            <button data-aos="zoom-in" data-aos-delay="200" className='bg-themeyellow px-6 py-3 rounded-lg font-semibold text-black '>
+              ONLINE COLLECTIONS
+            </button>
+          </div>
+        </div>
+        <div>
+          <div
+            className="w-full lg:px-20 px-5 lg:h-[700px] h-[600px] flex flex-col justify-center items-start gap-10 bg-cover bg-center "
+            style={{ backgroundImage: `url(${earbuds})` }}
+          >
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="50"
+              className="text-themeyellow border rounded-lg border-themeyellow px-6 py-2 text-xl"
+            >
+              Get upto Discounts 80% off
+            </h1>
+            <h2
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white lg:text-[120px] text-[60px] font-bold uppercase lg:leading-[120px] leading-[70px]"
+            >
+              wireless <br /> earbuds
+            </h2>
+            <h2
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white text-2xl"
+            >
+              100% trusted{" "}
+              <span className="text-themeyellow font-semibold">
+                Electronic Gadgets
+              </span>
+            </h2>
+            <button data-aos="zoom-in" data-aos-delay="200" className='bg-themeyellow px-6 py-3 rounded-lg font-semibold text-black '>
+              ONLINE COLLECTIONS
+            </button>
+          </div>
+        </div>
+        <div>
+          <div
+            className="w-full lg:px-20 px-5 lg:h-[700px] h-[600px] flex flex-col justify-center items-start gap-10 bg-cover bg-center "
+            style={{ backgroundImage: `url(${headset})` }}
+          >
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="50"
+              className="text-themeyellow border rounded-lg border-themeyellow px-6 py-2 text-xl"
+            >
+              Get upto Discounts 80% off
+            </h1>
+            <h2
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white lg:text-[120px] text-[60px] font-bold uppercase lg:leading-[120px] leading-[70px]"
+            >
+              wireless <br /> headset
+            </h2>
+            <h2
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white text-2xl"
+            >
+              100% trusted{" "}
+              <span className="text-themeyellow font-semibold">
+                Electronic Gadgets
+              </span>
+            </h2>
+            <button data-aos="zoom-in" data-aos-delay="200" className='bg-themeyellow px-6 py-3 rounded-lg font-semibold text-black '>
+              ONLINE COLLECTIONS
+            </button>
+          </div>
+        </div>
+      </Slider>
+    </div>
+  );
 }
 
 export default Hero
--- src/components/Types.jsx
+++ src/components/Types.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import banner1 from '../assets/banner1.jpg'
+import banner2 from '../assets/banner2.jpg'
+import banner3 from '../assets/banner3.jpg'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Types = () => {
+  useEffect(()=>{
+      AOS.init({
+        offset:100,
+        duration:500,
+        easing:"ease-in-out"
+      });
+      AOS.refresh();
+    },[])
   return (
     <div>Types</div>
   )
